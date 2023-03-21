1f438190b857c7de262b76f6262a62abbba69123
patrickmdey <patrickmdey@hotmail.com>
2023-03-21 11:37:52 -0300

    Feat: simple linear model (train-test)
    Co-authored-by: Matías Federico Lombardi <matiaslombardi@users.noreply.github.com>
    Co-authored-by: Ignacio Vazquez <igvazquez@users.noreply.github.com>

--- ej-obligatorio-1/main.py
+++ ej-obligatorio-1/main.py
@@ -5,34 +5,40 @@ from utils import *
 def main():
     df = pd.read_csv("./Advertising.csv", usecols=['TV','Radio','Newspaper','Sales'])
     
-    # corr_analysis(df)
-    # scatter_category(df, 'TV', 'Sales', 'Sales')
+    print("Running correlation analysis...\n")
+    corr_analysis(df)
+    print("Running simple linear regression...")
     # col_len = len(df['TV'])
     # linear_model(df, df['TV'].values.reshape(col_len, 1), df['Sales'].values.reshape(col_len, 1))
-    # simple_linear_models(df)
+    simple_linear_models(df)
 
-    # multiple_linear_models(df)
-    test(df)
+    print("Running multiple linear regression...")
+    multiple_linear_models(df)
+
+    print("Running simple linear regression test...")
+    simple_test(df)
+
+    print("Running multiple linear regression test...")
+    multiple_test(df)
 
 
 def simple_linear_models(df):
     col_len = len(df['TV'])
-
     for col in ['TV','Radio','Newspaper']:
-        linear_model(df[col].values.reshape(col_len, 1), df['Sales'].values.reshape(col_len, 1), True, col, 'Sales')
+        linear_model(df[col].values.reshape(col_len, 1), df['Sales'].values.reshape(col_len, 1), False, col, 'Sales')
 
 def multiple_linear_models(df):
     trimmed_df = df[["TV", "Radio", "Newspaper"]]
     linear_model(trimmed_df, df['Sales'], True)
 
 
-def test(df):
-    test_linear_model(df, 0.2)
+def simple_test(df):
+    for col in ['TV','Radio','Newspaper']:
+        test_simple_linear_model(df, col, 0.2)
 
+def multiple_test(df):
+    test_multple_linear_model(df, 0.2)
 
-    
-    
-    
 
 if __name__ == "__main__":
     main()
--- ej-obligatorio-1/utils.py
+++ ej-obligatorio-1/utils.py
@@ -15,8 +15,8 @@ def corr_analysis(df):
 def linear_model(X, y, multiple=False, x_label="null", y_label="null"):
     plt.clf()
     reg = LinearRegression().fit(X, y) # te calcula la recta de regresion
-    print("R^2 for " + ("multiple" if multiple else "linear"  + x_label) + ": " + str(reg.score(X, y))) # R^2
-    print("Bi for " + ("multiple" if multiple else "linear"  + x_label) + ": " + str(reg.coef_)) # coeficiente de la recta (b_i)
+    print("R^2 for " + ("multiple" if multiple else "linear "  + x_label) + ": " + str(reg.score(X, y))) # R^2
+    print("Bi for " + ("multiple" if multiple else "linear "  + x_label) + ": " + str(reg.coef_) + '\n') # coeficiente de la recta (b_i)
     if not multiple:
         plt.scatter(X, y)
         plt.xlabel(x_label)
@@ -24,9 +24,21 @@ def linear_model(X, y, multiple=False, x_label="null", y_label="null"):
         plt.plot(X, reg.predict(X), color='red', linewidth=2)
         plt.savefig("./out/"+x_label + "_vs_" + y_label + "_linear.png")
 
+def test_simple_linear_model(df, x_label, test_percentage=0.2):
+    train, test = train_test_split(df, test_size=test_percentage)
+
+    X_train = train[x_label].values.reshape(len(train[x_label]), 1)
+    y_train = train["Sales"]
+    X_test = test[x_label].values.reshape(len(test[x_label]), 1)
+    y_test = test["Sales"]
+    reg = LinearRegression().fit(X_train, y_train) # te calcula la recta de regresion
+    y_pred = reg.predict(X_test)
+    print("MSE for simple linear model (" + x_label +"): " + str(mean_squared_error(y_test, y_pred)))
+
+
 # TODO: faltaria hacerlo para cada variable por separado
-def test_linear_model(df, test_size=0.2):
-    train, test = train_test_split(df, test_size=0.2)
+def test_multple_linear_model(df, test_percentage=0.2):
+    train, test = train_test_split(df, test_size=test_percentage)
     X_train = train.drop("Sales", axis=1)
     y_train = train["Sales"]
     X_test = test.drop("Sales", axis=1)
