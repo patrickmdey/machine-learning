8fd470e8418b417151ac42fe1f272b11520ad018
patrickmdey <patrickmdey@hotmail.com>
2023-04-05 20:51:47 -0300

    Refactor: ejercicio 2
    Co-authored-by: Matías Federico Lombardi <matiaslombardi@users.noreply.github.com>
    Co-authored-by: Ignacio Vazquez <igvazquez@users.noreply.github.com>

--- TP1/ej2TextClassifier/main.py
+++ TP1/ej2TextClassifier/main.py
@@ -1,17 +1,19 @@
 import pandas as pd
 def main ():
     df = pd.read_csv("Noticias_argentinas.csv", 
-                     usecols=["fecha","titular","fuente","categoria"], 
-                     dtype={"fecha": "str", "titular": "str", "fuente": "str", "categoria": "str"})
+                     usecols=["fecha","titular","fuente","categoria"])
 
     title_list = df["titular"].tolist()
     counters = {}
     for title in title_list:
         title = title.lower()
+        replaceables = [".", ",", "!", "?", "(", ")", ":", ";", "-"]
+        for char in replaceables:
+            title = title.replace(char, "")
         words = title.split(" ")
         for word in words:
-            if len(word) <= 3:
-                continue
+            # if len(word) <= 3:
+            #     continue
             if word in counters:
                 counters[word] += 1
             else:
--- TP1/ej3BayesianNetwork/main.py
+++ TP1/ej3BayesianNetwork/main.py
@@ -3,7 +3,11 @@ import numpy as np
 import sys
 import json
 
+#Vector a = "x": [1, -1, -1, 0],
+#Vector b = "x": [2, 450, 3.5, 1],
+
 def main():
+
     new_instance = [1, 1, 1, 1]
 
     with open(sys.argv[1], 'r') as config_file:
@@ -18,9 +22,8 @@ def main():
                 exit()
         
     
-    new_instance[1] = 1 if new_instance[1] >= 500 else 0 
-    new_instance[2] = 1 if new_instance[2] >= 3 else 0 
-    print(new_instance)
+    new_instance[1] = 1 if new_instance[1] >= 500 else 0 if new_instance[1] >= 0 else -1
+    new_instance[2] = 1 if new_instance[2] >= 3 else 0 if new_instance[2] >= 0 else -1
 
     df = pd.read_csv("./binary.csv")
 
@@ -39,10 +42,9 @@ def main():
     gre_values = sorted(set(df['gre']))
     gpa_values = sorted(set(df['gpa']))
     admit_values = sorted(set(df['admit']))
-
     
     for value in rank_values:
-        rank_probs.append(len(df[df["rank"] == value]) / len(df))
+        rank_probs.append(len(df[df["rank"] == value]) / len(df)) # P(rank)
 
     for idx, gre in enumerate(gre_values):
         gre_probs.append([])
@@ -66,61 +68,31 @@ def main():
                 admit_probs[a_idx][gpa_idx].append([])
                 for rank in rank_values:
                     init_len = len(df[(df["rank"] == rank) & (df["gpa"] == gpa) & (df["gre"] == gre)])
-                    prob = len(df[(df['admit'] == admit) & (df["rank"] == rank) &
-                                     (df["gpa"] == gpa) & (df["gre"] == gre)]) / init_len
+                    conditional_len = len(df[(df['admit'] == admit) & (df["rank"] == rank) &
+                                     (df["gpa"] == gpa) & (df["gre"] == gre)])
+                    # TODO: capaz no hacerlo siempre
+                    prob =  (conditional_len + 1) / (init_len + 2)
                     admit_probs[a_idx][gpa_idx][gre_idx].append(prob)
-
     
+    ranks = [new_instance[0]] if new_instance[0] != -1 else rank_values
+    gres = [new_instance[1]] if new_instance[1] != -1 else gre_values
+    gpas = [new_instance[2]] if new_instance[2] != -1 else gpa_values
+
+    numerator = 0
+    denominator = 0
+    admit = new_instance[3]
+    for rank in ranks:
+        rank_prob = rank_probs[rank - 1]
+        for gre in gres:
+            gre_prob = gre_probs[gre][rank - 1]
+            for gpa in gpas:
+                gpa_prob = gpa_probs[gpa][rank - 1]
+                admit_prob = admit_probs[admit][gpa][gre][rank - 1] 
+
+                denominator += gre_prob * gpa_prob * rank_prob
+                numerator += gre_prob * gpa_prob * rank_prob * admit_prob
     
-    denominator = rank_probs[new_instance[0] - 1]
-    numerator = rank_probs[new_instance[0] - 1]
-
-    if new_instance[1] != -1 and new_instance[2] != -1:
-        # TODO: capaz armar un diccionario para no hacer el -1
-        denominator *= gre_probs[new_instance[1]][new_instance[0] - 1] * gpa_probs[new_instance[2]][new_instance[0] - 1]
-
-        numerator *= admit_probs[new_instance[3]][new_instance[2]][new_instance[1]][new_instance[0] - 1] * gre_probs[new_instance[1]][new_instance[0] - 1] * gpa_probs[new_instance[2]][new_instance[0] - 1]
-    
-    elif new_instance[1] != -1:
-        denominator *= gre_probs[1][new_instance[new_instance[0] - 1]]
-        numerator *= gre_probs[1][new_instance[new_instance[0] - 1]]
-        aux_den = 0
-        aux_num = 0
-        for idx, value in enumerate(gpa_probs):
-            aux_num += value[new_instance[0] - 1] * admit_probs[new_instance[3]][idx][new_instance[1]][new_instance[0] - 1]
-            aux_den += value
-        denominator *= aux_den
-        numerator *= aux_num
-    
-    elif new_instance[2] != -1:
-        denominator *= gpa_probs[2][new_instance[0] - 1]
-        numerator *= gpa_probs[2][new_instance[0] - 1]
-        aux_den = 0
-        aux_num = 0
-        for idx, value in enumerate(gre_probs):
-            aux_num += value[new_instance[0] - 1] * admit_probs[new_instance[3]][new_instance[2]][idx][new_instance[0] - 1]
-            aux_den += value[new_instance[0] - 1]
-        denominator *= aux_den
-    
-    else:
-        # r = 1 AND gpa = 0 AND gre = 0 ++++ r = 1 AND gpa = 0 AND gre = 1 ++++++
-        aux_den = 0
-        aux_num = 0
-        # print(gpa_probs)
-        for gpa_index, gpa_p in enumerate(gpa_probs):
-            for gre_index, gre_p in enumerate(gre_probs):
-                # aux_num = gpa_p * gre_p * admit_probs[new_instance[3]][gpa_index][gre_index][new_instance[0] - 1]
-                aux_num += gpa_p[new_instance[0] - 1] * gre_p[new_instance[0] - 1] * admit_probs[new_instance[3]][gpa_index][gre_index][new_instance[0] - 1]
-
-                aux_den += gpa_p[new_instance[0] - 1] * gre_p[new_instance[0] - 1]
-        denominator *= aux_den
-        numerator *= aux_num
-
-    print(numerator/denominator)
-
-    # TODO: falta LAPLACE, cual seria la cantidad de clases?
-
-    
+    print("Probability: " + str(numerator / denominator))    
     
 if __name__ == "__main__":
     main()
