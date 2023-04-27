f37dd1a27c2ad33b9b36cbddcf66c3f15cdbcfc7
patrickmdey <patrickmdey@hotmail.com>
2023-04-27 17:18:59 -0300

    Refactor + Feat: conditional df in node + start of random forest
    Co-authored-by: Matías Federico Lombardi <matiaslombardi@users.noreply.github.com>
    Co-authored-by: Ignacio Vazquez <igvazquez@users.noreply.github.com>

--- TP2/ej1RandomForest/main.py
+++ TP2/ej1RandomForest/main.py
@@ -1,10 +1,11 @@
 import json
-import sys
 
 import pandas as pd
-import numpy as np
 from preanalysis import categorize_columns
 from utils import *
+from sklearn.model_selection import train_test_split
+import numpy as np
+
 
 class Node:
     attr_name = ""
@@ -12,8 +13,9 @@ class Node:
     has_value = False
     children = {}
     parent = None
+    conditional_df = None
 
-    def __init__(self, parent, attr_name, attr_value=None, children=None, hasValue=None):
+    def __init__(self, parent, attr_name, attr_value=None, children=None, hasValue=None, conditional_df=None):
         if children is None:
             children = {}
         self.parent = parent
@@ -21,27 +23,29 @@ class Node:
         self.attr_value = attr_value
         self.children = children
         self.has_value = hasValue
+        self.conditional_df = conditional_df
 
 
 def create_tree(df, columns, target_column, parent):
     if len(df[target_column].unique()) == 1:
-        return Node(parent, target_column, df[target_column].unique()[0], {}, True)
+        return Node(parent, target_column, df[target_column].unique()[0], {}, True, df[target_column])
 
     # TODO:check condition
     if columns is None or len(columns) == 0:
-        return Node(parent, target_column, df[target_column].mode()[0], {}, True)
+        return Node(parent, target_column, df[target_column].mode()[0], {}, True, df.copy())
 
     gains = calculate_gains(df, columns)
     max_gain_attr = max(gains, key=gains.get)
     # max_gain_value = max(gains.values())
 
     root = Node(parent, max_gain_attr, None)
+    root.conditional_df = df.copy()
 
     for attr_value in df[max_gain_attr].unique():
         conditional_df = df[df[max_gain_attr] == attr_value]
         conditional_df = conditional_df.loc[:, conditional_df.columns != max_gain_attr]
 
-        child = Node(root, max_gain_attr, attr_value, {}, True)
+        child = Node(root, max_gain_attr, attr_value, {}, True, conditional_df.copy())
         root.children[attr_value] = child
 
         new_cols = [column_name for column_name in conditional_df.columns.tolist() if column_name != target_column]
@@ -76,7 +80,9 @@ def classify_instance(node, instance, target_column, df):
             instance_value = instance[node.attr_name]
             if instance_value not in children:
                 # print("ERROR: instance value not in children")
-                return rebuild_conditional_df(node, df, target_column)
+                # TODO: remove [] if it works
+                return node.conditional_df[target_column].mode()[0]
+                #return rebuild_conditional_df(node, df, target_column)
                 
             node = children[instance_value]
         else:
@@ -107,8 +113,9 @@ def prune_tree(root, max_nodes, df, target_column):
             # TODO: check
             if not node.has_value and node.parent is not None:
                 node = node.parent
-        
-            most_common_class = rebuild_conditional_df(node, df, target_column)
+
+            most_common_class = node.conditional_df[target_column].mode()[0]
+            #most_common_class = rebuild_conditional_df(node, df, target_column)
             node.children = {target_column: Node(node, target_column, most_common_class)}
 
 def count_nodes(root, target_column):
@@ -124,6 +131,31 @@ def count_nodes(root, target_column):
         nodes = children
     
     return amount
+
+def random_forest(df, target_column, test_percentage, examples_per_tree, tree_amount):
+    train, test = train_test_split(df, test_size=test_percentage)
+    train.reset_index(drop=True, inplace=True)
+    test.reset_index(drop=True, inplace=True)
+
+    trees = []
+    for _ in range(0, tree_amount):
+        tree = create_tree(train.sample(frac=1, replace=True), None, target_column, None)
+        prune_tree(tree, 80, df, target_column)
+        trees.append(tree)
+        
+    # TODO: change to apply
+    most_voted = []
+    for _, instance in test.iterrows():
+        predictions = []
+        for tree in trees:
+            predictions.append(classify_instance(tree, instance, target_column, df))
+        
+        if instance[target_column] == 0:
+            print("Predicciones " + str(predictions))
+            most_voted.append(np.bincount(predictions).argmax())
+            print("REAL: " + str(instance[target_column]))
+            print("PREDICHOOOO: " + str(most_voted[-1]))
+
 
 def id3(df, columns, target_column):
     partitions = partition_dataset(df, 0.1)
@@ -173,6 +205,7 @@ def main():
     attribute_columns = df.loc[:, df.columns != target_column].columns.tolist()
     if do_forest:
         print("Random Forest")
+        random_forest(df, target_column, 0.2, 80, 10)
     else:
         print("ID3")
         id3(df, attribute_columns, target_column)
