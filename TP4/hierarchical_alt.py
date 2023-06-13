879519df5f866c94dae1cb2174da5e5d9ac29c65
patrickmdey <patrickmdey@hotmail.com>
2023-06-12 21:25:28 -0300

    Feat: hierarchial_alt
    Co-authored-by: Matías Federico Lombardi <matiaslombardi@users.noreply.github.com>
    Co-authored-by: Ignacio Vazquez <igvazquez@users.noreply.github.com>

---
+++ TP4/hierarchical_alt.py
@@ -0,0 +1,99 @@
+import numpy as np
+import math
+class Group:
+    id = 0
+    def __init__(self, element):
+        self.elements = [element]
+        self.distances_to = {}
+        self.id = Group.id
+        Group.id += 1
+        self.centroid = element # TODO: esto solo si lo hacemos con el average, podriamos hacer pruebas con max y min
+    
+    def add_element(self, group):
+        #FIXME: es un array en realidad
+        np.concatenate(self.elements, group.elements)
+        # self.observations.append(group)
+        self.centroid = np.mean(self.elements, axis=0)
+    
+    def calculate_distance_to(self, other_group):
+        distance = np.linalg.norm(self.centroid - other_group.centroid)
+        self.distances_to[other_group.id] = np.linalg.norm(self.centroid - other_group.centroid)
+        return distance
+    
+    def calculate_avg_centroid(self):
+        return self.centroid
+
+class HierarchicalGroups:
+    def __init__(self, k, observations):
+        self.k = k
+        self.observations = observations
+        self.distances = {}
+    
+    def calculate_and_group(self, groups):
+        
+        return
+    
+    def solve(self):
+        clusters = [] # TODO: maybe map?
+        for idx, observation in enumerate(self.observations):
+            #clusters[idx] = Group(observation)
+            clusters.append(Group(observation))
+
+        distances = np.zeros((len(clusters), len(clusters))) # esta escrito asi porque es shape
+
+        for i in range(len(clusters)):
+            for j in range(i + 1, len(clusters)):
+                distance = clusters[i].calculate_distance_to(clusters[j])
+                distances[i][j] = distance
+                distances[j][i] = distance
+
+        np.fill_diagonal(distances, math.inf) # TODO: check
+
+        current_clusters = len(clusters)
+        while current_clusters > self.k:
+            min_distance = np.min(distances)
+            min_distance_idxs = np.where(distances == min_distance)
+            
+            min_distance_idxs = min_distance_idxs[0]
+            print(min_distance_idxs)
+            first_group = min(min_distance_idxs) 
+            second_group = max(min_distance_idxs) 
+
+            # TODO: chequear que te viene
+            print(len(clusters))
+            print(second_group)
+            
+            to_remove = clusters.pop(second_group)
+            clusters[first_group].add_element(to_remove)
+
+            # TODO: borrar el segundo grupo ya que fue anexado pero poder mantener los indices
+            np.delete(distances, second_group, axis=0)
+            np.delete(distances, second_group, axis=1)
+
+
+            for i, cluster in enumerate(clusters):
+                distance = clusters[i].calculate_distance_to(clusters[first_group])
+                distances[i][first_group] = distance
+                distances[first_group][i] = distance
+            
+            distances[first_group][first_group] = math.inf
+            current_clusters -= 1
+
+        return clusters
+    
+
+    def create_dendogram(self):
+        groups = []
+        for observation in self.observations:
+            groups.append(observation)
+
+        # TODO: debe haber una mejor forma de calcular la menor distancia
+        # TODO: buscar alguna forma de no calcular de nuevo cuentas ya hechas
+        # TODO: tambien necesitas las distancias desde afuera
+        while len(groups) > 1:
+            for first_group in groups:
+                for second_group in groups:
+                    if first_group.id != second_group.id:
+                        first_group.calculate_distance_to(second_group)
+                
+        
--- TP4/main.py
+++ TP4/main.py
@@ -6,9 +6,11 @@ import sys
 import os
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from hierarchical_alt import HierarchicalGroups
 from utils import boxplot_column
 from kohonen_network import KohonenNetwork
 from kmeans import Kmeans
+
 
 def plot_elbow(variations):
     plt.clf()
@@ -19,6 +21,7 @@ def plot_elbow(variations):
     if not os.path.exists("./out/kmeans"):
         os.makedirs("./out/kmeans")
     plt.savefig("./out/kmeans/elbow.png")
+
 
 def plot_heatmap(matrix, title, labels=None):
     plt.clf()
@@ -31,68 +34,88 @@ def plot_heatmap(matrix, title, labels=None):
 
     plt.show()
 
+
 def dataset_analysis(df, useful_cols):
     print("Amount of duplicated rows:", df.duplicated().sum())
     print("Amount of null values:", df.isnull().sum().sum())
     print("Amount of rows:", df.shape[0])
     print("Amount of columns:", df.shape[1])
-    print("Duplicated rows:")
-    print(df[df.duplicated()]["imdb_id"])
+    # print("Duplicated rows:")
+    # print(df[df.duplicated()]["imdb_id"])
 
     for col in useful_cols:
         boxplot_column(df, col)
 
+    # remove rows with null values
+    df = df.dropna()
+    df = df.drop_duplicates(subset=useful_cols, keep=False)
+
+    years = df["release_date"].str.split("-", n=2, expand=True)[0]
+    df["release_date"] = years
+
+    trimed_df = df[useful_cols]
+
+    # df.drop(["genres", "imdb_id", "original_title", "overview", "production_companies"], axis=1)
+    # print(df["original_title"].head())
+
+    # get standard deviation per column
+    print(df.std)
+
 
 def run_kohonen(df, params):
-        rows = np.array(df.values.tolist())
-        headers = df.columns.values.tolist()
-        # group_by = df["genres"]
+    rows = np.array(df.values.tolist())
+    headers = df.columns.values.tolist()
+    # group_by = df["genres"]
 
-        # rows = rows.astype(float)
+    # rows = rows.astype(float)
 
-        # rows = StandardScaler().fit_transform(rows) #http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html
+    # rows = StandardScaler().fit_transform(rows) #http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html
 
-        grid_k = params['init_k']
-        radius = params['init_r']
+    grid_k = params['init_k']
+    radius = params['init_r']
 
-        network = KohonenNetwork(eta=params['eta'], grid_k=grid_k, radius=radius)
+    network = KohonenNetwork(eta=params['eta'], grid_k=grid_k, radius=radius)
 
-        network.solve(rows)
+    network.solve(rows)
 
-        winners_associated = np.zeros(dtype=int, shape=(grid_k, grid_k))
-        winners = network.find_all_winners(rows)
-        labels = np.empty(dtype="U256", shape=(grid_k, grid_k))
-        for idx, (row, col) in enumerate(winners):
-            winners_associated[row][col] += 1
-            # labels[row][col] += group_by[idx] + "\n"
+    winners_associated = np.zeros(dtype=int, shape=(grid_k, grid_k))
+    winners = network.find_all_winners(rows)
+    labels = np.empty(dtype="U256", shape=(grid_k, grid_k))
+    for idx, (row, col) in enumerate(winners):
+        winners_associated[row][col] += 1
+        # labels[row][col] += group_by[idx] + "\n"
 
-        plot_heatmap(winners_associated, "Generos", winners_associated)
-        plot_heatmap(winners_associated, "Agrupación de Generos", labels)
-        plot_heatmap(network.u_matrix(), "Matriz U")
+    plot_heatmap(winners_associated, "Generos", winners_associated)
+    plot_heatmap(winners_associated, "Agrupación de Generos", labels)
+    plot_heatmap(network.u_matrix(), "Matriz U")
 
 
 if __name__ == '__main__':
 
-    # TODO: chequear exactamente que variables vamos a usar para comparar
-    useful_cols = ["budget","popularity", "production_companies", "production_countries", "revenue", "runtime", "spoken_languages", "vote_average", "vote_count"]
-    dataset_analysis(pd.read_csv("movie_data.csv", sep=';'), useful_cols)
+    method = 'hierarchical'
+    # analysis_cols = ["budget", "popularity", "production_companies", "production_countries",
+    #                "revenue", "runtime", "spoken_languages", "vote_average", "vote_count"]
 
-    method = 'kmeans'
+    # # TODO: chequear exactamente que variables vamos a usar para comparar
+    # # useful_cols = analysis_cols + ["release_date"]
 
-    df = pd.read_csv('movie_data.csv', sep=';', usecols=useful_cols)
+    # # dataset_analysis(pd.read_csv("movie_data.csv", sep=';'), analysis_cols)
 
-    #remove rows with null values
-    df = df.dropna()
-    df = df.drop_duplicates(subset=useful_cols, keep=False)
-    
-    # # normalize df
-    # TODO: esto lo hicimos con un scaler, adaptarlo
-    df = (df - df.mean()) / (df.max() - df.min())
-    #scaler = MinMaxScaler()
-    #df.loc[:, ['wordcount', 'titleSentiment', 'sentimentValue']] = scaler.fit_transform(df[['wordcount', 'titleSentiment', 'sentimentValue']].values)
 
-    # df["genres"] = pd.read_csv('movie_data.csv', sep=';', usecols=["genres"])
-    
+    # df = pd.read_csv('movie_data.csv', sep=';', usecols=analysis_cols)
+
+    # # years = df["release_date"].str.split("-", n=2, expand=True)[0]
+    # # print(years)
+    # # df["release_date"] = years
+
+    # # remove rows with null values
+    # df = df.dropna()
+    # df = df.drop_duplicates(subset=analysis_cols, keep=False)
+
+    # # # normalize df
+    # # TODO: esto lo hicimos con un scaler, adaptarlo
+    # df = (df - df.mean()) / (df.max() - df.min())
+
     if method == 'kohonen':
         params = {
             'eta': 0.1,
@@ -118,6 +141,10 @@ if __name__ == '__main__':
 
         print(variations)
         plot_elbow(variations)
-    
+
     elif method == 'hierarchical':
-        pass
+        data = np.array([[0.4, 0.53], [0.22, 0.38], [0.35, 0.32], [0.26, 0.19], [0.08, 0.41], [0.45, 0.3]])
+        her = HierarchicalGroups(3, data)
+        clusters = her.solve()
+        for i, cluster in enumerate(clusters):
+            print(f"Cluster {i + 1}: {cluster.elements}")
